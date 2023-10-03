c93693f7fc1c5d8f264fdcb558dd8be6ba71e27e
XantreGodlike <valerik007.vs@gmail.com>
2023-10-03 10:49:06 +0400

    added useLinkedSignal deep unwrap test

--- packages/utils/src/__tests__/hooks/useLinkedSignal.test.tsx
+++ packages/utils/src/__tests__/hooks/useLinkedSignal.test.tsx
@@ -31,21 +31,32 @@ describe.concurrent("useLinkedSignal()", () => {
   );
 
   itRenderer("should unwrap signals", async ({ reactRoot, act, expect }) => {
-    const shouldChooseFirst = signal(false);
+    const chooseSignal = signal(false);
     const sig1 = signal(0);
     const sig2 = signal(-10);
     await reactRoot().render(
       React.createElement(() => {
-        const linked = useLinkedSignal(shouldChooseFirst.value ? sig1 : sig2);
+        const linked = useLinkedSignal(chooseSignal.value ? sig1 : sig2);
 
-        expect(linked.peek()).toBe(shouldChooseFirst.peek() ? 0 : -10);
+        expect(linked.peek()).toBe(chooseSignal.peek() ? 0 : -10);
 
         return null;
       })
     );
 
     await act(() => {
-      shouldChooseFirst.value = true;
+      chooseSignal.value = true;
     });
+  });
+
+  itRenderer("should unwrap deeply", async ({ reactRoot, act, expect }) => {
+    const sig1 = signal(signal(signal(0)));
+    await reactRoot().render(
+      React.createElement(() => {
+        expect(useLinkedSignal(sig1).peek()).toBe(0);
+
+        return null;
+      })
+    );
   });
 });
