03318956d916e890c6e76f096600253cce0e3087
XantreGodlike <valerik007.vs@gmail.com>
2023-12-08 00:14:47 +0100

    completely changed approach

--- packages/react/swc/src/lib.rs
+++ packages/react/swc/src/lib.rs
@@ -1,17 +1,23 @@
-#![feature(box_patterns)]
+#![feature(box_patterns, let_chains, if_let_guard)]
 
 use regex::Regex;
 use swc_core::{
+    common::comments::Comments,
     common::{sync::Lazy, DUMMY_SP},
     ecma::{
         ast::*,
         atoms::Atom,
         parser::{EsConfig, Syntax},
-        visit::{as_folder, noop_visit_mut_type, FoldWith, VisitMut, VisitMutWith},
+        visit::{
+            as_folder, noop_visit_mut_type, FoldWith, Visit, VisitMut, VisitMutWith, VisitWith,
+        },
+    },
+    plugin::{
+        plugin_transform,
+        proxies::{PluginCommentsProxy, TransformPluginProgramMetadata},
     },
 };
 pub extern crate swc_core;
-use swc_core::plugin::{plugin_transform, proxies::TransformPluginProgramMetadata};
 
 /// Test transformation.
 #[macro_export]
@@ -45,31 +51,47 @@ macro_rules! test {
     };
 }
 
-enum SignalsStatus {
-    OptIn,
-    OptOut,
+enum TransformMode {
     Auto,
+    Manual,
+    All,
 }
 
-#[derive(PartialEq, Eq, Clone, Copy)]
-enum Context {
-    Root,
-    Function,
-    RenderFunction,
-    ComponentLikeNameDeclr,
-    ComponentLike,
-    Component,
-}
-
-pub struct TransformVisitor {
-    context: Context,
+pub struct TransformVisitor<C>
+where
+    C: Comments,
+{
+    comments: C,
+    mode: TransformMode,
     should_add_import: bool,
-    signals: SignalsStatus,
 }
 
 fn is_component_name(name: &str) -> bool {
     static RE: Lazy<Regex> = Lazy::new(|| Regex::new("^[A-Z]").unwrap());
     RE.is_match(name)
+}
+
+trait MaybeComponentName {
+    fn is_component_name(&self) -> bool;
+}
+
+impl MaybeComponentName for Ident {
+    fn is_component_name(&self) -> bool {
+        is_component_name(self.sym.as_str())
+    }
+}
+impl MaybeComponentName for BindingIdent {
+    fn is_component_name(&self) -> bool {
+        self.id.is_component_name()
+    }
+}
+impl MaybeComponentName for Pat {
+    fn is_component_name(&self) -> bool {
+        if let Pat::Ident(id) = self {
+            return id.is_component_name();
+        }
+        false
+    }
 }
 
 trait FunctionLikeExpr {
@@ -97,179 +119,197 @@ impl FunctionLikeExpr for FnDecl {
     }
 }
 
-fn can_be_component_function(expr: &Expr) -> bool {
+fn wrap_with_use_signals(n: &Vec<Stmt>) -> Vec<Stmt> {
+    let mut new_stmts = Vec::new();
+    let signal_effect_ident = Ident {
+        span: DUMMY_SP,
+        sym: Atom::from("_s"),
+        optional: false,
+    };
+    new_stmts.push(Stmt::Decl(Decl::Var(Box::new(VarDecl {
+        span: DUMMY_SP,
+        kind: VarDeclKind::Var,
+        declare: false,
+        decls: vec![VarDeclarator {
+            definite: false,
+            span: DUMMY_SP,
+            init: Some(Box::new(Expr::Call(CallExpr {
+                span: DUMMY_SP,
+                callee: Callee::Expr(Box::new(Expr::Ident(Ident {
+                    span: DUMMY_SP,
+                    sym: Atom::from("useSignals"),
+                    optional: false,
+                }))),
+                args: vec![],
+                type_args: None,
+            }))),
+            name: Pat::Ident(BindingIdent {
+                id: signal_effect_ident.clone(),
+                type_ann: None,
+            }),
+        }],
+    }))));
+    new_stmts.push(Stmt::Try(Box::new(TryStmt {
+        span: DUMMY_SP,
+        block: BlockStmt {
+            span: DUMMY_SP,
+            stmts: n.to_vec(),
+        },
+        handler: None,
+        finalizer: Some(BlockStmt {
+            span: DUMMY_SP,
+            stmts: vec![Stmt::Expr(ExprStmt {
+                span: DUMMY_SP,
+                expr: Box::new(Expr::Call(CallExpr {
+                    args: vec![],
+                    span: DUMMY_SP,
+                    type_args: None,
+                    callee: Callee::Expr(Box::new(Expr::Member(MemberExpr {
+                        span: DUMMY_SP,
+                        prop: MemberProp::Ident(Ident {
+                            span: DUMMY_SP,
+                            sym: Atom::from("f"),
+                            optional: false,
+                        }),
+                        obj: Box::new(Expr::Ident(signal_effect_ident)),
+                    }))),
+                })),
+            })],
+        }),
+    })));
+
+    new_stmts
+}
+
+enum FunctionLike<'a> {
+    Arrow(&'a mut ArrowExpr),
+    Fn(&'a mut FnExpr),
+}
+
+fn extract_fn_from_expr<'a>(expr: &'a mut Expr) -> Option<FunctionLike<'a>> {
     match expr {
-        Expr::Fn(it) => it.is_regular(),
-        Expr::Arrow(it) => it.is_regular(),
-        _ => false,
+        Expr::Fn(fn_expr) if fn_expr.is_regular() => Some(FunctionLike::Fn(fn_expr)),
+        Expr::Arrow(arrow_expr) if arrow_expr.is_regular() => Some(FunctionLike::Arrow(arrow_expr)),
+        Expr::Call(CallExpr {
+            args,
+            span: _,
+            type_args: _,
+            callee: _,
+        }) => {
+            if let [ExprOrSpread {
+                spread: None,
+                expr: first_arg_expr,
+            }] = args.as_mut_slice()
+            {
+                extract_fn_from_expr(first_arg_expr.unwrap_parens_mut())
+            } else {
+                None
+            }
+        }
+        _ => None,
     }
 }
 
-impl VisitMut for TransformVisitor {
-    noop_visit_mut_type!();
-    // Implement necessary visit_mut_* methods for actual custom transform.
-    // A comprehensive list of possible visitor methods can be found here:
-    // https://rustdoc.swc.rs/swc_ecma_visit/trait.VisitMut.html
-    fn visit_mut_var_declarator(&mut self, n: &mut VarDeclarator) {
-        match (&n.init, &n.name) {
-            (Some(exp), Pat::Ident(ident))
-                if is_component_name(ident.to_string().as_str())
-                    && (can_be_component_function(exp)) =>
-            {
-                let old_context = self.context;
-                self.context = Context::ComponentLikeNameDeclr;
-                n.visit_mut_children_with(self);
-                self.context = old_context;
-            }
-            _ => {
-                let old_context = self.context;
-                self.context = Context::Function;
-                n.visit_mut_children_with(self);
-                match self.context {
-                    Context::RenderFunction => self.context = Context::Component,
-                    _ => self.context = old_context,
-                }
-            }
+trait Blockable {
+    fn to_block(&mut self) -> BlockStmt;
+}
+impl Blockable for BlockStmtOrExpr {
+    fn to_block(&mut self) -> BlockStmt {
+        match self {
+            BlockStmtOrExpr::BlockStmt(block) => block.to_owned(),
+            BlockStmtOrExpr::Expr(expr) => BlockStmt {
+                span: DUMMY_SP,
+                stmts: vec![Stmt::Return(ReturnStmt {
+                    span: DUMMY_SP,
+                    arg: Some(expr.clone()),
+                })],
+            },
         }
     }
-    fn visit_mut_arrow_expr(&mut self, n: &mut ArrowExpr) {
-        let old_context = self.context;
-        match old_context {
-            Context::ComponentLikeNameDeclr => {
-                self.context = Context::ComponentLike;
-            }
-            _ => {
-                self.context = Context::Function;
+}
+struct HasJSX {
+    found: bool,
+}
+impl Visit for HasJSX {
+    fn visit_jsx_element(&mut self, _: &JSXElement) {
+        self.found = true;
+    }
+}
+
+fn has_jsx<N>(n: &N) -> bool
+where
+    N: VisitWith<HasJSX>,
+{
+    let mut v = HasJSX { found: false };
+    n.visit_children_with(&mut v);
+    v.found
+}
+
+impl FunctionLike<'_> {
+    fn has_jsx(&self) -> bool {
+        match self {
+            FunctionLike::Arrow(arrow_expr) => has_jsx(*arrow_expr),
+            FunctionLike::Fn(fn_expr) => has_jsx(*fn_expr),
+        }
+    }
+    fn has_name(&self) -> bool {
+        match self {
+            FunctionLike::Arrow(_) => false,
+            FunctionLike::Fn(fn_expr) => fn_expr.ident.is_some(),
+        }
+    }
+}
+
+impl<C> VisitMut for TransformVisitor<C>
+where
+    C: Comments,
+{
+    noop_visit_mut_type!();
+    fn visit_mut_var_decl(&mut self, n: &mut VarDecl) {
+        if let [first] = n.decls.as_mut_slice()
+            && first.name.is_component_name()
+            && let Some(init) = &mut first.init
+            && let Some(component) = extract_fn_from_expr(init.unwrap_parens_mut())
+            && !component.has_name()
+            && component.has_jsx()
+        {
+            match component {
+                FunctionLike::Arrow(arrow_expr) => {
+                    let mut block = arrow_expr.body.to_block();
+                    let wrapped_body = wrap_with_use_signals(&block.stmts);
+                    block.stmts = wrapped_body;
+                    arrow_expr.body = Box::new(BlockStmtOrExpr::BlockStmt(block.to_owned()));
+                }
+                FunctionLike::Fn(fn_expr) => {
+                    if let Some(block_stmt) = &mut fn_expr.function.body {
+                        block_stmt.stmts = wrap_with_use_signals(&block_stmt.stmts);
+                    } else {
+                    }
+                }
             }
         }
 
         n.visit_mut_children_with(self);
-        self.context = old_context
     }
     fn visit_mut_fn_decl(&mut self, n: &mut FnDecl) {
-        let old_context = self.context;
-        match old_context {
-            Context::ComponentLikeNameDeclr => n.visit_mut_children_with(self),
-            _ if n.is_regular() && is_component_name(n.ident.to_string().as_str()) => {
-                self.context = Context::ComponentLikeNameDeclr;
-                n.visit_mut_children_with(self);
-                self.context = old_context
-            }
-            _ => {}
+        if n.ident.is_component_name()
+            && has_jsx(n)
+            && let Some(block_stmt) = &mut n.function.body
+        {
+            block_stmt.stmts = wrap_with_use_signals(&block_stmt.stmts);
         }
-    }
-    fn visit_mut_function(&mut self, n: &mut Function) {
-        let old_context = self.context;
-
-        match old_context {
-            Context::ComponentLikeNameDeclr => {
-                self.context = Context::ComponentLike;
-            }
-            _ => {
-                self.context = Context::Function;
-            }
-        }
-
         n.visit_mut_children_with(self);
-        self.context = old_context
-    }
-    fn visit_mut_stmts(&mut self, n: &mut Vec<Stmt>) {
-        let old_context = self.context;
-        match old_context {
-            Context::ComponentLike => {
-                n.visit_mut_children_with(self);
-                if self.context != Context::Component {
-                    self.context = old_context;
-                    return;
-                }
-
-                self.context = old_context;
-                let mut new_stmts = Vec::new();
-                let signal_effect_ident = Ident {
-                    span: DUMMY_SP,
-                    sym: Atom::from("_s"),
-                    optional: false,
-                };
-                new_stmts.push(Stmt::Decl(Decl::Var(Box::new(VarDecl {
-                    span: DUMMY_SP,
-                    kind: VarDeclKind::Var,
-                    declare: false,
-                    decls: vec![VarDeclarator {
-                        definite: false,
-                        span: DUMMY_SP,
-                        init: Some(Box::new(Expr::Call(CallExpr {
-                            span: DUMMY_SP,
-                            callee: Callee::Expr(Box::new(Expr::Ident(Ident {
-                                span: DUMMY_SP,
-                                sym: Atom::from("useSignals"),
-                                optional: false,
-                            }))),
-                            args: vec![],
-                            type_args: None,
-                        }))),
-                        name: Pat::Ident(BindingIdent {
-                            id: signal_effect_ident.clone(),
-                            type_ann: None,
-                        }),
-                    }],
-                }))));
-                new_stmts.push(Stmt::Try(Box::new(TryStmt {
-                    span: DUMMY_SP,
-                    block: BlockStmt {
-                        span: DUMMY_SP,
-                        stmts: n.to_vec(),
-                    },
-                    handler: None,
-                    finalizer: Some(BlockStmt {
-                        span: DUMMY_SP,
-                        stmts: vec![Stmt::Expr(ExprStmt {
-                            span: DUMMY_SP,
-                            expr: Box::new(Expr::Call(CallExpr {
-                                args: vec![],
-                                span: DUMMY_SP,
-                                type_args: None,
-                                callee: Callee::Expr(Box::new(Expr::Member(MemberExpr {
-                                    span: DUMMY_SP,
-                                    prop: MemberProp::Ident(Ident {
-                                        span: DUMMY_SP,
-                                        sym: Atom::from("f"),
-                                        optional: false,
-                                    }),
-                                    obj: Box::new(Expr::Ident(signal_effect_ident)),
-                                }))),
-                            })),
-                        })],
-                    }),
-                })));
-
-                *n = new_stmts;
-            }
-            _ => {
-                n.visit_mut_children_with(self);
-            }
-        }
-    }
-
-    fn visit_mut_jsx_element(&mut self, n: &mut JSXElement) {
-        match self.context {
-            Context::Function => {
-                self.context = Context::RenderFunction;
-            }
-            Context::ComponentLike => {
-                self.context = Context::Component;
-            }
-            _ => {}
-        }
-
-        n.visit_mut_children_with(self)
     }
 }
 
-fn get_visitor() -> TransformVisitor {
+fn get_visitor<C>(c: C) -> TransformVisitor<C>
+where
+    C: Comments,
+{
     TransformVisitor {
-        signals: SignalsStatus::Auto,
-        context: Context::Root,
         should_add_import: false,
+        comments: c,
+        mode: TransformMode::All,
     }
 }
 
@@ -290,7 +330,7 @@ fn get_visitor() -> TransformVisitor {
 /// Refer swc_plugin_macro to see how does it work internally.
 #[plugin_transform]
 pub fn process_transform(program: Program, _metadata: TransformPluginProgramMetadata) -> Program {
-    program.fold_with(&mut as_folder(get_visitor()))
+    program.fold_with(&mut as_folder(get_visitor(PluginCommentsProxy)))
 }
 
 fn get_syntax() -> Syntax {
@@ -305,7 +345,7 @@ fn get_syntax() -> Syntax {
 // unless explicitly required to do so.
 test!(
     get_syntax(),
-    |_| as_folder(get_visitor()),
+    |_| as_folder(get_visitor(PluginCommentsProxy)),
     boo,
     // Input codes
     r#"const A = () => {
@@ -317,6 +357,14 @@ test!(
     }
     return <div />
 };
+const Cecek = () => <div />
+const Cec = memo(() => {
+    return <div />
+})
+const Cyc = React.memo(() => {
+    return <div />
+})
+const CycPlain = () => 5
 
 function B(){
     return <div />
