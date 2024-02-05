ccc6bfd0c5490fc4be75e1b8b8acd3c5e008bb2d
XantreGodlike <valerik007.vs@gmail.com>
2024-02-05 20:36:49 +0100

    clean up

--- packages/type/src/t.ts
+++
@@ -1,7 +0,0 @@
-import { ReactDOM } from "react";
-import "./initial";
-type C = ReactDOM["div"];
-type D = Parameters<C>[0];
-type E = NonNullable<D>["onDoubleClick"];
-//   ^?
-type C2 = JSX.IntrinsicElements["div"];
