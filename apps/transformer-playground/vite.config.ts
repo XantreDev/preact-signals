eed1d81a02ebe25a50436479c4956810c80ce2a8
XantreGodlike <valerik007.vs@gmail.com>
2024-03-31 17:58:56 +0200

    wip

---
+++ apps/transformer-playground/vite.config.ts
@@ -0,0 +1,16 @@
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [
+    react({
+      babel: {
+        plugins: [
+          "module:@preact-signals/safe-react/babel",
+          "module:@preact-signals/utils/babel",
+        ],
+      },
+    }),
+  ],
+});
