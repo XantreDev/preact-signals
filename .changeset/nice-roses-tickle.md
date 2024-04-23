6b0943980e1f0046efc2b6cdadbad35cf9a5e0ae
XantreDev <valerik007.vs@gmail.com>
2024-04-23 14:39:26 +0200

    fix mission export

---
+++ .changeset/nice-roses-tickle.md
@@ -0,0 +1,5 @@
+---
+"@preact-signals/query": patch
+---
+
+Export `useQueryClient$`
--- packages/query/src/react-query/index.ts
+++ packages/query/src/react-query/index.ts
@@ -1,31 +1,32 @@
 /* istanbul ignore file */
 
 // Side effects
-import './setBatchUpdatesFn'
+import "./setBatchUpdatesFn";
 
 // Re-export core
-export * from '@tanstack/query-core'
+export * from "@tanstack/query-core";
 
 // React Query
-export * from './types'
-export { useQueries } from './useQueries'
-export type { QueriesResults, QueriesOptions } from './useQueries'
-export { useQuery } from './useQuery'
+export * from "./types";
+export { useQueries } from "./useQueries";
+export type { QueriesResults, QueriesOptions } from "./useQueries";
+export { useQuery } from "./useQuery";
 export {
   defaultContext,
   QueryClientProvider,
   useQueryClient,
-} from './QueryClientProvider'
-export type { QueryClientProviderProps } from './QueryClientProvider'
-export type { QueryErrorResetBoundaryProps } from './QueryErrorResetBoundary'
-export { useHydrate, Hydrate } from './Hydrate'
-export type { HydrateProps } from './Hydrate'
+  useQueryClient$,
+} from "./QueryClientProvider";
+export type { QueryClientProviderProps } from "./QueryClientProvider";
+export type { QueryErrorResetBoundaryProps } from "./QueryErrorResetBoundary";
+export { useHydrate, Hydrate } from "./Hydrate";
+export type { HydrateProps } from "./Hydrate";
 export {
   QueryErrorResetBoundary,
   useQueryErrorResetBoundary,
-} from './QueryErrorResetBoundary'
-export { useIsFetching } from './useIsFetching'
-export { useIsMutating } from './useIsMutating'
-export { useMutation } from './useMutation'
-export { useInfiniteQuery } from './useInfiniteQuery'
-export { useIsRestoring, IsRestoringProvider } from './isRestoring'
+} from "./QueryErrorResetBoundary";
+export { useIsFetching } from "./useIsFetching";
+export { useIsMutating } from "./useIsMutating";
+export { useMutation } from "./useMutation";
+export { useInfiniteQuery } from "./useInfiniteQuery";
+export { useIsRestoring, IsRestoringProvider } from "./isRestoring";
