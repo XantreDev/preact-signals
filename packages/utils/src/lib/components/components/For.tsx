89842b31e334b1d50c62d915282b49b0f1772e14
XantreDev <valerik007.vs@gmail.com>
2024-06-24 23:34:07 +0200

    wip

--- packages/utils/src/lib/components/components/For.tsx
+++ packages/utils/src/lib/components/components/For.tsx
@@ -1,4 +1,11 @@
-import { Fragment, cloneElement, createElement, isValidElement,  } from "react";
+import {
+  Fragment,
+  ReactNode,
+  cloneElement,
+  createElement,
+  isValidElement,
+  useRef,
+} from "react";
 import {
   useComputedOnce,
   useSignalOfReactive,
@@ -6,6 +13,11 @@ import {
 } from "../../hooks";
 import type { GetValue, Reactive } from "../../utils";
 import { RenderResult } from "../type";
+import {
+  ReadonlySignal,
+  Signal,
+  untracked,
+} from "@preact-signals/unified-signals";
 
 type GetArrItemValue<T extends Reactive<any[]>> = GetValue<T>[number];
 
@@ -55,6 +67,45 @@ export const For = <T extends Reactive<any[]>>({
           );
         })
   ).value as JSX.Element;
+};
+
+// if order is changed -> make full bail out
+
+/**
+ * @useSignals
+ */
+export const Index = <T extends Reactive<unknown[]>>(props: {
+  each: T;
+  fallback?: ReactNode;
+  children: (item: GetArrItemValue<T>) => ReactNode;
+}) => {
+  const eachComputed = useSignalOfReactive(props.each);
+  const length = useComputedOnce(() => eachComputed.value.length);
+  const prevLength = useRef(length.peek());
+  let sigs: Signal<T>[] = [];
+  let prevKey: number[] = [];
+  let currentItems: JSX.Element[] = [];
+
+  untracked(() => {
+    if (length.value !== currentItems.length) {
+      if (length.value === 0) {
+        sigs = [];
+        prevKey = [];
+        currentItems = [];
+        return props.fallback;
+      }
+      const newItems = Array.from({ length: length.value });
+      const prevKeysMap = new Map<Signal, number>()
+
+      for (let i = 0; i < currentItems.length; ++i) {
+        if (i < sigs.length && sigs[i]?.value === eachComputed.value[i]) {
+          newItems[i] = currentItems[i];
+        } else if (i < sigs.length) {
+          sigs[i] = 
+        }
+      }
+    }
+  });
 };
 
 // const renderToNode =
